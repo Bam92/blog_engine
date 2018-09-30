d8554ad9502d4b7d63d270a8d305679154249a18
abelmbula <abelmbula@bam.com>
2018-09-30 13:11:41 +0200

    layout of error page
    add an image to make fun

--- view/frontend/errorView.php
+++ view/frontend/errorView.php
@@ -1,5 +1,13 @@
-<?php $title= 'Mon Blog Erreur';
+<?php $title= 'Mon Blog Erreur';?>
 
-    echo "<h1>" .$errorMessage ."</h1>";
-
+    <div class="row" id="errorPanel">
+        <div class="col-xs-5">
+            <img class="img-responsive pull-right" src="public/img/404-ghost.png" alt="Error ghost">
+        </div>
+    <div class="col-xs-6">
+        <?php echo "<h1>Whoops...<br><small>" .$errorMessage ."</small></h1>";?>
+    </div>
+    </div>
+<?php
  require('template.php');
+?>
