093b51255f5ed68ace2d2ad5e685ad1d7950dc2c
abelmbula <abelmbula@bam.com>
2018-10-31 10:45:57 +0200

    add html5shiv and IE8.js

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -5,7 +5,14 @@
     <title><?= $title ?></title>
     <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/style.css">
+    
+    <!--[if IE]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <!--[if lte IE 7]>
+    <script src="js/IE8.js" type="text/javascript"></script><![endif]-->
+    <!--[if lt IE 7]>
 
+    <link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/><![endif]-->
 
 
   </head>
