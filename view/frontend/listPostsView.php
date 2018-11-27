48aab24f1b96031640923f6fd44beffb248b7813
abelmbula <abelmbula@bam.com>
2018-11-27 07:44:45 +0200

    feat: (home page front) add link to admin page
    once the user clicks connexion nav menu, call login page

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -3,9 +3,9 @@
 <?php ob_start(); ?>
 
 <div class="jumbotron jumbotron-fluid">
-    <?php while ($row = $unique->fetch()) { ?>
-    
-  <div class="container">
+   <?php while ($row = $unique->fetch()) { ?>
+
+   <div class="container">
       <header class="entry-header">
           <h1>
             <a href="index.php?action=post&amp;id=<?php echo $row ['pst_id']; ?>"><?php echo $row ['pst_title']; ?></a>
@@ -16,41 +16,43 @@
     <div class="entry-content">
       <?php echo nl2br(substr($row['pst_content'], 0, 300)); ?>...
     </div>
-    </div>
-    <?php } ?>
+   </div>
+   <?php } ?>
 </div>
 
 <?php while ($row = $posts->fetch()) { ?>
 
-<article class="container">
-  <header class="entry-header">
-    <h1>
-      <a href="index.php?action=post&amp;id=<?php echo $row['pst_id']; ?>">
-        <?php echo ($row['pst_title']); ?>
-      </a>
-    </h1>
-  </header>
-  <!--,entry-header -->
+<div class="container">
+   <article id="posts" class="offset-md-1">
+     <header class="entry-header">
+       <h1>
+         <a href="index.php?action=post&amp;id=<?php echo $row['pst_id']; ?>">
+           <?php echo ($row['pst_title']); ?>
+         </a>
+       </h1>
+     </header>
+     <!--,entry-header -->
 
-  <div class="entry-content">
-    <?php echo nl2br(substr($row['pst_content'], 0, 300)); ?>...
-  </div>
-  <!--,entry-content -->
+     <div class="entry-content">
+       <?php echo nl2br(substr($row['pst_content'], 0, 300)); ?>...
+     </div>
+     <!--,entry-content -->
 
-  <footer class="entry-footer">
-    <span class="posted-on"><?php echo $row['pst_date_fr']; ?></span>
-    <span class="by">Abel</span>
-    <span class=""> <?php
-      //while ($tag = $tags->fetch()) {
-        print($tags['tag_name']);
-    //  }
-    ?>
-    <?php echo $cmt_number['nb']; ?>
-    </span>
-  </footer>
-  <!--,entry-footer -->
+     <footer class="entry-footer">
+       <span class="posted-on"><?php echo $row['pst_date_fr']; ?></span>
+       <span class="by">Abel</span>
+       <span class=""> <?php
+         //while ($tag = $tags->fetch()) {
+           print($tags['tag_name']);
+       //  }
+       ?>
+       <?php echo $cmt_number['nb']; ?>
+       </span>
+     </footer>
+     <!--,entry-footer -->
 
-</article>
+   </article>
+</div>
 
 <?php } ?>
 
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -35,7 +35,7 @@
                 <a class="nav-link" href="#">Contact</a>
               </li>
                 <li class="nav-item">
-                <a class="nav-link" href="#">Connexion</a>
+                <a class="nav-link" href="index.php?action=login">Connexion</a>
               </li>
             </ul>
               
